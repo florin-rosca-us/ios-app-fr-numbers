b8150f3dc3afafb2e2c7317f8a00b0d658d6a1c5
Florin Rosca <florin-rosca-us@users.noreply.github.com>
2016-08-15 11:20:40 -0700

    Adding a Share Extension

---
+++ FRAudio/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  FRAudio
+//
+//  Created by Florin on 8/15/16.
+//  Copyright © 2016 Florin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
