35083ffad181d4dd55817252210322cbfc45d3b5
Florin Rosca <florin-rosca-us@users.noreply.github.com>
2016-08-23 17:47:28 -0700

    Adding a table view controller

---
+++ FRAudio/FRNumberTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FRNumberTableViewController.h
+//  FRNumbers
+//
+//  Created by Florin on 8/23/16.
+//  Copyright © 2016 Florin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRNumberTableViewController : UITableViewController
+
+@end
